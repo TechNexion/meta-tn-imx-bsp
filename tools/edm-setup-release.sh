f24cb8bd6eed6fe658796598626460f3329e9ded
Richard Hu <richard.hu@technexion.com>
2018-10-11 14:37:56 +0800

    tools/edm-setup-release.sh: apply patch to add '--no-sandbox' as default argument of chromium
    Avoid the error 'Running as root without --no-sandbox is not supported.',
    when activating chromium.

--- tools/edm-setup-release.sh
+++ tools/edm-setup-release.sh
@@ -56,6 +56,8 @@ clean_up()
 patch -Np1 -r - sources/meta-fsl-bsp-release/imx/meta-bsp/recipes-bsp/imx-mkimage/imx-boot_0.2.bb < sources/meta-edm-bsp-release/patches/0001-imx-boot-pass-dtb-name-to-imx-mkimage-when-making-fl.patch
 patch -Np1 -r - sources/meta-fsl-bsp-release/imx/meta-bsp/recipes-security/optee-imx/optee-os-imx_git.bb < sources/meta-edm-bsp-release/patches/0002-optee-os-imx-fix-build-failure-when-the-board-isn-t-.patch
 patch -Np1 -r - sources/meta-fsl-bsp-release/imx/meta-bsp/classes/image_types_fsl.bbclass < sources/meta-edm-bsp-release/patches/0003-image_types_fsl.bbclass-change-to-put-u-boot.img-int.patch
+cp sources/meta-edm-bsp-release/patches/0001-add-no-sandbox-as-argument-by-default.patch sources/meta-browser/recipes-browser/chromium/files
+patch -Np1 -r - sources/meta-browser/recipes-browser/chromium/chromium-gn.inc < sources/meta-edm-bsp-release/patches/0004-chromium-add-no-sandbox-as-default-argument.patch
 
 # get command line options
 OLD_OPTIND=$OPTIND
