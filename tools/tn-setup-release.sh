6af269f73c10ed43edffc7fa736b93d5cf732d9c
Po Cheng <po.cheng@technexion.com>
2020-07-24 15:30:17 +0800

    Migrate setup scripts from yocto 2.5
    Signed-off-by: Po Cheng <po.cheng@technexion.com>

--- tools/tn-setup-release.sh
+++ tools/tn-setup-release.sh
@@ -20,10 +20,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-. sources/meta-imx/tools/setup-utils.sh
-
 CWD=`pwd`
-PROGNAME="setup-environment"
+PROGNAME="$CWD/sources/meta-tn-imx-bsp/tools/setup-environment.sh"
 exit_message ()
 {
    echo "To return to this build environment later please run:"
@@ -33,7 +31,7 @@ exit_message ()
 
 usage()
 {
-    echo -e "\nUsage: source imx-setup-release.sh
+    echo -e "\nUsage: source tn-setup-release.sh
     Optional parameters: [-b build-dir] [-h]"
 echo "
     * [-b build-dir]: Build directory, if unspecified script uses 'build' as output directory
@@ -44,7 +42,9 @@ echo "
 
 clean_up()
 {
-
+    unset OLD_OPTIND
+    unset OPTIND
+    unset CPU_TYPE
     unset CWD BUILD_DIR FSLDISTRO
     unset fsl_setup_help fsl_setup_error fsl_setup_flag
     unset usage clean_up
@@ -60,7 +60,7 @@ while getopts "k:r:t:b:e:gh" fsl_setup_flag
 do
     case $fsl_setup_flag in
         b) BUILD_DIR="$OPTARG";
-           echo -e "\n Build directory is " $BUILD_DIR
+           echo -e "\nBuild directory: $BUILD_DIR"
            ;;
         h) fsl_setup_help='true';
            ;;
@@ -131,18 +131,30 @@ cd -
 # Override the click-through in meta-freescale/EULA
 FSL_EULA_FILE=$CWD/sources/meta-imx/EULA.txt
 
-# Set up the basic yocto environment
-if [ -z "$DISTRO" ]; then
-   DISTRO=$FSLDISTRO MACHINE=$MACHINE . ./$PROGNAME $BUILD_DIR
+# Set up the basic yocto environment by calling our setup-environment.sh
+if [ -n "$FSLDISTRO" ]; then
+  echo "TechNexion Setup BSP Release: source TechNexion setup-environment.sh wrapper scripts"
+  echo "    MACHINE=$MACHINE FSLDISTRO=$FSLDISTRO source $PROGNAME $BUILD_DIR"
+  echo ""
+  DISTRO=$FSLDISTRO MACHINE=$MACHINE source $PROGNAME $BUILD_DIR
+elif [ -n "$DISTRO" ]; then
+  echo "TechNexion Setup BSP Release: Source TechNexion setup-environment.sh wrapper scripts"
+  echo "    MACHINE=$MACHINE DISTRO=$DISTRO source $PROGNAME $BUILD_DIR"
+  echo ""
+  DISTRO=$DISTRO MACHINE=$MACHINE source $PROGNAME $BUILD_DIR
 else
-   MACHINE=$MACHINE . ./$PROGNAME $BUILD_DIR
+  echo "TechNexion Setup BSP Release: Source TechNexion setup-environment.sh wrapper scripts"
+  echo "    MACHINE=$MACHINE source $PROGNAME $BUILD_DIR"
+  echo ""
+  MACHINE=$MACHINE source $PROGNAME $BUILD_DIR
 fi
 
-# Point to the current directory since the last command changed the directory to $BUILD_DIR
+echo -e "\nTechNexion Setup BSP Release: Further modification to local.conf and bblayers.conf"
+# Point to the current directory since the source setup-environment.sh changed the directory to $BUILD_DIR
 BUILD_DIR=.
 
 if [ ! -e $BUILD_DIR/conf/local.conf ]; then
-    echo -e "\n ERROR - No build directory is set yet. Run the 'setup-environment' script before running this script to create " $BUILD_DIR
+    echo -e "\nERROR - No build directory is set yet. Run the 'setup-environment' script before running this script to create " $BUILD_DIR
     echo -e "\n"
     return 1
 fi
@@ -166,49 +178,92 @@ else
     cp $BUILD_DIR/conf/bblayers.conf.org $BUILD_DIR/conf/bblayers.conf
 fi
 
-
-META_FSL_BSP_RELEASE="${CWD}/sources/meta-imx/meta-bsp"
-
-echo "" >> $BUILD_DIR/conf/bblayers.conf
-echo "# i.MX Yocto Project Release layers" >> $BUILD_DIR/conf/bblayers.conf
-hook_in_layer meta-imx/meta-bsp
-hook_in_layer meta-imx/meta-sdk
-hook_in_layer meta-imx/meta-ml
-
-echo "" >> $BUILD_DIR/conf/bblayers.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-browser\"" >> $BUILD_DIR/conf/bblayers.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-rust\"" >> $BUILD_DIR/conf/bblayers.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-gnome\"" >> $BUILD_DIR/conf/bblayers.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-networking\"" >> $BUILD_DIR/conf/bblayers.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-filesystems\"" >> $BUILD_DIR/conf/bblayers.conf
-
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-qt5\"" >> $BUILD_DIR/conf/bblayers.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-python2\"" >> $BUILD_DIR/conf/bblayers.conf
-
-# Add technexion bsp layers to bblayers.conf
-echo "" >> $PWD/conf/bblayers.conf
-echo "# setup Technexion i.MX Yocto Project Release Layers in bblayers.conf" | tee -a $PWD/conf/bblayers.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-tn-imx-bsp\"" >> $PWD/conf/bblayers.conf
-
-if [ -d ../sources/meta-ivi ]; then
-    echo -e "\n## Genivi layers" >> $BUILD_DIR/conf/bblayers.conf
-    echo "BBLAYERS += \"\${BSPDIR}/sources/meta-gplv2\"" >> $BUILD_DIR/conf/bblayers.conf
-    echo "BBLAYERS += \"\${BSPDIR}/sources/meta-ivi/meta-ivi\"" >> $BUILD_DIR/conf/bblayers.conf
-    echo "BBLAYERS += \"\${BSPDIR}/sources/meta-ivi/meta-ivi-bsp\"" >> $BUILD_DIR/conf/bblayers.conf
-    echo "BBLAYERS += \"\${BSPDIR}/sources/meta-ivi/meta-ivi-test\"" >> $BUILD_DIR/conf/bblayers.conf
+# Pass in the extra variables for uEnv.txt recipe
+if [ -n "$TOKEN" ]; then
+  echo "Specified PA_TOKEN: $TOKEN"
+  export PA_TOKEN=$TOKEN
+  if ! grep -qF "PA_TOKEN" <<< $BB_ENV_EXTRAWHITE; then
+    echo "Export PA_TOKEN=$TOKEN to yocto via BB_ENV_EXTRAWHITE"
+    export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE PA_TOKEN"
+  fi
+fi
+if [ -n "$DISPLAY" ]; then
+  echo "Specified DISPLAY_INFO: $DISPLAY"
+  export DISPLAY_INFO=$DISPLAY
+  if ! grep -qF "DISPLAY_INFO" <<< $BB_ENV_EXTRAWHITE; then
+    echo "Export DISPLAY_INFO=$DISPLAY to yocto via BB_ENV_EXTRAWHITE"
+    export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE DISPLAY_INFO"
+  fi
+fi
+if [ -n "$PANEL" ]; then
+    export DISPLAY_PANEL=$PANEL
+    export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE DISPLAY_PANEL"
+fi
+if [ -n "$BASEBOARD" ]; then
+  echo "Specified BASE_BOARD: $BASEBOARD"
+  export BASE_BOARD=$BASEBOARD
+  if ! grep -qF "BASE_BOARD" <<< $BB_ENV_EXTRAWHITE; then
+    echo "Export BASE_BOARD=$BASEBOARD to yocto via BB_ENV_EXTRAWHITE"
+    export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE BASE_BOARD"
+  fi
+fi
+if [ -n "$FDTNAME" ]; then
+    export ALT_FDTNAME=$FDTNAME
+    export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE ALT_FDTNAME"
 fi
 
-echo BSPDIR=$BSPDIR
-echo BUILD_DIR=$BUILD_DIR
+# Identify SOC type
+CPU_TYPE=$(echo $MACHINE | sed 's/.*-\(imx[5-8][a-z]*\)[- $]*.*/\1/g')
+
+# Choose corresponding firmware package for different WLAN (QCA or BRCM), e.g. 'linux-firmware-brcm-tn' or 'linux-firmware-qca-tn'
+if [ -z "${WIFI_FIRMWARE#"${WIFI_FIRMWARE%%[! ]*}"}" ]; then
+    echo "WARNING - No WIFI_FIRMWARE specified"
+    RF_FIRMWARES=""
+else
+    if [ "$WIFI_FIRMWARE" == "all" ]; then
+        if [ "$CPU_TYPE" == 'imx8mq' ] || [ "$CPU_TYPE" == 'imx8mm' ]; then
+            echo "WARNING - imx8mq/imx8mm SOM only supports qca wireless module, so load qca firmware"
+            RF_FIRMWARES="qca ath-pci"
+        elif [ "$CPU_TYPE" == 'imx6' ] || [ "$CPU_TYPE" == "imx7" ] || [ "$CPU_TYPE" == 'imx6ul' ]; then
+            RF_FIRMWARES="qca brcm ath-pci"
+        else
+            echo "WARNING - No matched CPU_TYPE: $CPU_TYPE, hence no WIFI_FIRMWARE"
+            RF_FIRMWARES=""
+        fi
+    elif [ "$WIFI_FIRMWARE" == "y" ] || [ "$WIFI_FIRMWARE" == "Y" ]; then
+        if [ "$CPU_TYPE" == 'imx8mq' ] || [ "$CPU_TYPE" == 'imx8mm' ]; then
+            echo "WARNING - imx8mq/imx8mm SOM only supports qca wireless module, so load qca firmware"
+            RF_FIRMWARES="qca"
+        else
+            if  [ -z "${WIFI_MODULE#"${WIFI_MODULE%%[! ]*}"}" ]; then
+                echo "WARNING - No WIFI_MODULE specified."
+                RF_FIRMWARES=""
+            else
+                RF_FIRMWARES=$WIFI_MODULE
+            fi
+        fi
+    else
+        echo "WARNING - Unrecognized WIFI_FIRMWARE specified"
+        RF_FIRMWARES=""
+    fi
+fi
+if [ -n $RF_FIRMWARES ]; then
+  echo "Specified wifi firmwares: $RF_FIRMWARES"
+  export RF_FIRMWARES=$RF_FIRMWARES
+  if ! grep -qF "RF_FIRMWARES" <<< $BB_ENV_EXTRAWHITE; then
+    echo "Export RF_FIRMWARES=$RF_FIRMWARES to yocto via BB_ENV_EXTRAWHITE"
+    export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE RF_FIRMWARES"
+  fi
+fi
 
 # Support integrating community meta-freescale instead of meta-fsl-arm
 if [ -d ../sources/meta-freescale ]; then
-    echo meta-freescale directory found
+    echo "meta-freescale directory found, so use freescale community bsp bblayer instead."
     # Change settings according to environment
     sed -e "s,meta-fsl-arm\s,meta-freescale ,g" -i conf/bblayers.conf
     sed -e "s,\$.BSPDIR./sources/meta-fsl-arm-extra\s,,g" -i conf/bblayers.conf
 fi
 
-cd  $BUILD_DIR
+cd $BUILD_DIR
 clean_up
-unset FSLDISTRO
+
