847b0bc33fec9d60693c0eb2100c8df2f896bb9c
Po Cheng <po.cheng@technexion.com>
2021-01-04 18:04:23 +0800

    tn-growpart: update tn-first-boot script and include e2fsprogs-resize2fs to tn-image-multimedia-full to ensure it is built and packaged
    Signed-off-by: Po Cheng <po.cheng@technexion.com>

--- recipes-extended/growpart/growpart/tn-first-boot.sh
+++ recipes-extended/growpart/growpart/tn-first-boot.sh
@@ -4,22 +4,26 @@
 # Date: 11/25/2020       #
 ##########################
 
+CPID=""
 MMC_DEV=$(lsblk | grep mmcblk | head -n 1 |  awk '{print $1}')
-ROOTFS_SIZE=$(lsblk | grep "$MMC_DEV"p2 | awk '{print $4}' | awk -F. '{print $1}')
+TOTAL_SIZE=$(lsblk -b | grep "$MMC_DEV " | awk '{print $4}')
+ROOTFS_SIZE=$(lsblk -b | grep "$MMC_DEV"p2 | awk '{print $4}')
+# spare = total - rootfs - 500MB
+SPARE_SIZE=$((($TOTAL_SIZE-$ROOTFS_SIZE)-524288000))
 
-if [ $ROOTFS_SIZE -lt 4 ]; then
+if [ $SPARE_SIZE -gt 0 -a $SPARE_SIZE -gt $ROOTFS_SIZE ]; then
 
   sleep 5
 
-  if [[ "$(cat /proc/device-tree/model  | grep "AXON-IMX8MP")" ]] || [[ "$(cat /proc/device-tree/model  | grep "EDM-G-IMX8MP")" ]] ; then
-    gst-launch-1.0 -v filesrc location=/etc/xdg/weston/tn-standby.jpg ! decodebin ! imagefreeze ! autovideosink &
-  elif [[ "$(cat /proc/device-tree/model  | grep "PICO-IMX8MM")" ]]; then
-    gst-launch-1.0 -v filesrc location=/etc/xdg/weston/tn-standby.jpg ! decodebin ! imagefreeze ! glimagesink render-rectangle="<1,1,1280,720>" &
-  elif [[ "$(cat /proc/device-tree/model  | grep "PICO-IMX8MQ")" ]] || [[ "$(cat /proc/device-tree/model  | grep "EDM-IMX8MQ")" ]] ; then
-    if [[ "$(grep -rn "ili9881c" /proc/device-tree/)" ]]; then
-      gst-launch-1.0 -v filesrc location=/etc/xdg/weston/tn-standby.jpg ! jpegdec ! imagefreeze ! glimagesink render-rectangle="<1,1,1280,720>" &
+  if grep -q "AXON-IMX8MP" /proc/device-tree/model || grep -q "EDM-G-IMX8MP" /proc/device-tree/model ; then
+    gst-launch-1.0 -v filesrc location=/etc/xdg/weston/tn-standby.jpg ! decodebin ! imagefreeze ! autovideosink & CPID="$!"
+  elif grep -q "PICO-IMX8MM" /proc/device-tree/model ; then
+    gst-launch-1.0 -v filesrc location=/etc/xdg/weston/tn-standby.jpg ! decodebin ! imagefreeze ! glimagesink render-rectangle="<1,1,1280,720>" & CPID="$!"
+  elif grep -q "PICO-IMX8MQ" /proc/device-tree/model || grep -q "EDM-IMX8MQ" /proc/device-tree/model ; then
+    if grep -q -rn "ili9881c" /proc/device-tree/ ; then
+      gst-launch-1.0 -v filesrc location=/etc/xdg/weston/tn-standby.jpg ! jpegdec ! imagefreeze ! glimagesink render-rectangle="<1,1,1280,720>" & CPID="$!"
     else
-      gst-launch-1.0 -v filesrc location=/etc/xdg/weston/tn-standby.jpg ! jpegdec ! imagefreeze ! glimagesink render-rectangle="<1,1,1920,1080>" &
+      gst-launch-1.0 -v filesrc location=/etc/xdg/weston/tn-standby.jpg ! jpegdec ! imagefreeze ! glimagesink render-rectangle="<1,1,1920,1080>" & CPID="$!"
     fi
   fi
   sleep 5
@@ -30,9 +34,9 @@ if [ $ROOTFS_SIZE -lt 4 ]; then
     resize2fs /dev/"$MMC_DEV"p2
     sync
     sleep 2
-
-    PID=$(ps aux | grep "gst-launch" | sed -n 2p | awk '{print $2}')
-    kill -9 "$PID"
+    if ps -p $CPID > /dev/null; then
+      kill -9 $CPID
+    fi
     reboot
   fi
 fi
