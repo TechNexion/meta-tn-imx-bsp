3b7d3de4eaf8a2d66d4121e03df243a6d59debd5
Po Cheng <po.cheng@technexion.com>
2021-01-21 17:09:33 +0800

    tsl: use weston-init to init weston compositor with black background and no toolbar
    Signed-off-by: Po Cheng <po.cheng@technexion.com>

--- recipes-graphics/wayland/weston-init/setup-weston-init.sh
+++ recipes-graphics/wayland/weston-init/setup-weston-init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 get_mode () {
+  IFNAME=${1##*/}
   RESOL=$(cat $1/modes | head -n 1)
   WIDTH=${RESOL//x*}
   HEIGHT=${RESOL//*x}
@@ -10,9 +11,14 @@ for card in /sys/class/drm/card?-*
 do
   get_mode $card
   if [ -f /etc/xdg/weston/weston.ini -a -n $RESOL ]; then
-    if ! grep -q "^name=${card#*-}" /etc/xdg/weston/weston.ini ; then
+    # check existing core section for cardX
+    if grep -q "^drm-device=" /etc/xdg/weston/weston.ini; then
+      sed -e 's,drm-device.*,drm-device='${IFNAME%%-*}',g' -i /etc/xdg/weston/weston.ini
+    fi
+    # enable cardX output section
+    if ! grep -q "^name=${IFNAME#*-}" /etc/xdg/weston/weston.ini ; then
       echo -e "\n[output]" >> /etc/xdg/weston/weston.ini
-      echo "name=${card#*-}" >> /etc/xdg/weston/weston.ini
+      echo "name=${IFNAME#*-}" >> /etc/xdg/weston/weston.ini
       echo "mode=${RESOL}@60" >> /etc/xdg/weston/weston.ini
       if [ $HEIGHT -gt $WIDTH ]; then
         echo "transform=rotate-270" >> /etc/xdg/weston/weston.ini
