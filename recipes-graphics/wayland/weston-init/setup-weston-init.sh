c4bb42552d1b6af1b873f44667fce1dd84fb9c4d
Richard Hu <richard.hu@technexion.com>
2021-04-21 05:13:54 +0000

    weston: improve rotation detection mechanism
    If the interface name and resolution in weston.ini doesn't match the settings detected by DRM,
    clear the old settings in westion.ini and create a new one.

--- recipes-graphics/wayland/weston-init/setup-weston-init.sh
+++ recipes-graphics/wayland/weston-init/setup-weston-init.sh
@@ -15,14 +15,24 @@ do
     if grep -q "^drm-device=" /etc/xdg/weston/weston.ini; then
       sed -e 's,drm-device.*,drm-device='${IFNAME%%-*}',g' -i /etc/xdg/weston/weston.ini
     fi
-    # enable cardX output section
-    if ! grep -q "^name=${IFNAME#*-}" /etc/xdg/weston/weston.ini ; then
-      echo -e "\n[output]" >> /etc/xdg/weston/weston.ini
-      echo "name=${IFNAME#*-}" >> /etc/xdg/weston/weston.ini
-      echo "mode=${RESOL}@60" >> /etc/xdg/weston/weston.ini
-      if [ $HEIGHT -gt $WIDTH ]; then
-        echo "transform=rotate-270" >> /etc/xdg/weston/weston.ini
-      fi
+
+    if ! grep -q "^name=${IFNAME#*-}" /etc/xdg/weston/weston.ini || ! grep -q "^mode=${RESOL}" /etc/xdg/weston/weston.ini ; then
+        # If the interface name and resolution in weston.ini doesn't match the settings detected by DRM,
+        # then, clear the old settings in westion.ini and create a new one.
+
+        # clear [output] display section
+        sed -i '/^\[output\]/,/^$/d' /etc/xdg/weston/weston.ini
+
+        # enable cardX output section
+        echo >> /etc/xdg/weston/weston.ini
+        echo -e "[output]" >> /etc/xdg/weston/weston.ini
+        echo "name=${IFNAME#*-}" >> /etc/xdg/weston/weston.ini
+        echo "mode=${RESOL}@60" >> /etc/xdg/weston/weston.ini
+        if [ $HEIGHT -gt $WIDTH ]; then
+          echo "transform=rotate-270" >> /etc/xdg/weston/weston.ini
+        fi
+        sync
     fi
+
   fi
 done
