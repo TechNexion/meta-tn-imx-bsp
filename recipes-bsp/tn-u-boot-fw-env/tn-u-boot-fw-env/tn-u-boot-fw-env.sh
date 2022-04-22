bcc5609bbb4835f17bf19454e8e05f81f5cc2513
Richard Hu <richard.hu@technexion.com>
2022-04-22 10:02:51 +0800

    recipes-bsp/tn-u-boot-fw-env: improve error handling when MMC device information isn't in boot argument

--- recipes-bsp/tn-u-boot-fw-env/tn-u-boot-fw-env/tn-u-boot-fw-env.sh
+++ recipes-bsp/tn-u-boot-fw-env/tn-u-boot-fw-env/tn-u-boot-fw-env.sh
@@ -2,6 +2,11 @@
 MMC_TAR=$(cat /proc/cmdline | grep -io "mmcblk[[:digit:]]")
 ARCH=$(uname -m)
 
+if [ -z "$MMC_TAR" ]; then
+       echo "Can not find MMC device path from kernel argument!"
+       exit 1
+fi
+
 if [ ${ARCH} == 'aarch64' ];then
        echo -e "/dev/${MMC_TAR}\t0x400000\t0x4000" > /etc/fw_env.config
 else
