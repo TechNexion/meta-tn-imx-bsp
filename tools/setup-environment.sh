9fc7ff8402dea6022cd61fd235135e771e86f2c1
Richard Hu <richard.hu@technexion.com>
2022-05-31 13:56:29 +0800

    tools/setup-environment.sh: update to to match meta-layer changes in Yocto 3.4(Honister)
    Update bblayers to adapt to the changes in 'meta-imx/tools/imx-setup-release.sh'

--- tools/setup-environment.sh
+++ tools/setup-environment.sh
@@ -140,7 +140,7 @@ if [ ${TNCONFIGS} != 0 -o ${FSLCONFIGS} != 0 ]; then
       echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-networking\"" >> $PWD/conf/bblayers.conf
       echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-filesystems\"" >> $PWD/conf/bblayers.conf
 
-      echo "BBLAYERS += \"\${BSPDIR}/sources/meta-qt5\"" >> $PWD/conf/bblayers.conf
+      echo "BBLAYERS += \"\${BSPDIR}/sources/meta-qt6\"" >> $PWD/conf/bblayers.conf
       echo "BBLAYERS += \"\${BSPDIR}/sources/meta-python2\"" >> $PWD/conf/bblayers.conf
 
       # Enable docker for mx8 machines
