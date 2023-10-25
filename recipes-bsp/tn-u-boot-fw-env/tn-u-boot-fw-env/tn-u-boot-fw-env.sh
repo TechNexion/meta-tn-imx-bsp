15b1bccec941260fd919a24651d8cdaaa5ce409b
Joe Zhang <joe.zhang@technexion.com>
2023-10-25 18:29:37 +0800

    recipes-bsp/tn-u-boot-fw-env: add support for qspi boot env

--- recipes-bsp/tn-u-boot-fw-env/tn-u-boot-fw-env/tn-u-boot-fw-env.sh
+++ recipes-bsp/tn-u-boot-fw-env/tn-u-boot-fw-env/tn-u-boot-fw-env.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 MMC_TAR=$(cat /proc/cmdline | grep -io "mmcblk[[:digit:]]")
+QSPI_BOOT=$(cat /proc/cmdline  |grep -Poi "qspi_boot=\w*" |cut -d '=' -f 2)
 ARCH=$(uname -m)
 
 if [ -z "$MMC_TAR" ]; then
-       echo "Can not find MMC device path from kernel argument!"
-       exit 1
+	echo "Can not find MMC device path from kernel argument!"
+	exit 1
+fi
+
+if [[ ${QSPI_BOOT} == 'yes' ]];then
+	BOOT_DEV='mtd0'
+else
+	BOOT_DEV=${MMC_TAR}
 fi
 
 if [ ${ARCH} == 'aarch64' ];then
-       echo -e "/dev/${MMC_TAR}\t0x400000\t0x4000" > /etc/fw_env.config
+	echo -e "/dev/${BOOT_DEV}\t0x400000\t0x4000" > /etc/fw_env.config
 else
-       echo -e "/dev/${MMC_TAR}\t0xc0000\t0x2000" > /etc/fw_env.config
+	echo -e "/dev/${BOOT_DEV}\t0xc0000\t0x2000" > /etc/fw_env.config
 fi
-
