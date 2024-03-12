3369f352a809a677aec27dcad85873771d2f1f83
Joe Zhang <joe.zhang@technexion.com>
2024-03-12 15:11:11 +0800

    recipes-tn/enable-lanbypass-eth: rewrite clock behavior

--- recipes-tn/enable-lanbypass-eth/files/enable_lanbypass_eth.sh
+++ recipes-tn/enable-lanbypass-eth/files/enable_lanbypass_eth.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-LANBYPASS_PWM_BLOCK="30660000"
-PERIOD="400000000"      # 0.4 sec
-DUTY_CYCLE="200000000"  # 0.2 sec
+generate_clock () {
+	while true
+	do
+		echo 1 > /sys/class/gpio/gpio${1}/value
+		sleep 0.2
+		echo 0 > /sys/class/gpio/gpio${1}/value
+		sleep 0.2
+	done
+}
 
-PWMCHIP=$(find / -name "*pwm*" | grep ${LANBYPASS_PWM_BLOCK} | grep -Poi "pwmchip\d$")
+GPIO_NUM=$(cat /sys/kernel/debug/gpio | grep "LANBYPASS_gen_clock" | grep -Poi "gpio-\d+" | cut -d '-' -f 2)
+echo ${GPIO_NUM} > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio${GPIO_NUM}/direction
 
-if [[ -z ${PWMCHIP} ]];then
-	echo "" > /dev/console
-	echo "$0: Can't find the pwm block of ${LANBYPASS_PWM_BLOCK}.pwm" > /dev/console
-	echo "$0: Ethernet might not work due to this error!!!" > /dev/console
-	exit
-fi
+generate_clock ${GPIO_NUM} &
 
-cd /sys/class/pwm/${PWMCHIP}
-
-ls |grep pwm0 > /dev/null
-if [[ $? -ne 0 ]];then
-	echo 0 > export
-fi
-
-cd pwm0/
-echo ${PERIOD} > period
-echo ${DUTY_CYCLE} > duty_cycle
-
-echo 1 > enable
+sleep 5
