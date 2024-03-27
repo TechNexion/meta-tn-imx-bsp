3e734fc18d645af0bcdece2522cc933b7e2ea824
Joe Zhang <joe.zhang@technexion.com>
2024-03-27 08:16:04 +0000

    recipes-tn/power-key-listener: replace regex to busybox type

--- recipes-tn/power-key-listener/files/power_key_listener.sh
+++ recipes-tn/power-key-listener/files/power_key_listener.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DEVICE=/dev/input/event0
+INPUT_INDEX=$(dmesg |grep gpio_keys | grep -Eoi "input[[:digit:]]" | rev| cut -c 1)
+DEVICE=/dev/input/event${INPUT_INDEX}
 
 INFO="POWER KEY Listener:"
 
@@ -20,7 +21,10 @@ toggle_led(){
 
 do_long_pressed_job(){
         # reset device
-        gpioset 1 5=0
+	# gpiochip1=32~63, number=5 =>37
+	echo 37 > /sys/class/gpio/export
+	echo out > /sys/class/gpio/gpio37/direction
+	echo 0 > /sys/class/gpio/gpio37/value
 }
 
 do_job(){
