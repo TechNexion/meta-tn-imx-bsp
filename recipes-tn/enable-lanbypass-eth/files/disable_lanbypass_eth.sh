80d57ab15729e46eb9ea306e8c1c26c2aa38de74
Joe Zhang <joe.zhang@technexion.com>
2024-03-27 08:15:28 +0000

    recipes-tn/enable-lanbypass-eth: replace regex to busybox type

--- recipes-tn/enable-lanbypass-eth/files/disable_lanbypass_eth.sh
+++ recipes-tn/enable-lanbypass-eth/files/disable_lanbypass_eth.sh
@@ -3,7 +3,7 @@
 PID=$(ps -aux |grep "/usr/sbin/enable_lanbypass_eth.sh" | tr -s ' '| cut -d  ' ' -f 2)
 kill -STOP ${PID}
 
-GPIO_NUM=$(cat /sys/kernel/debug/gpio | grep "LANBYPASS_gen_clock" | grep -Poi "gpio-\d+" | cut -d '-' -f 2)
+GPIO_NUM=$(cat /sys/kernel/debug/gpio | grep "LANBYPASS_gen_clock" | grep -Eoi "[[:digit:]]{1,}" | cut -d '-' -f 2)
 echo ${GPIO_NUM} > /sys/class/gpio/unexport
 
 sleep 5
